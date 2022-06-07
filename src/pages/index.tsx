58596c8dd4e6c3c01082e5e27e86078e95a3c326
devalecf <afessler1998@gmail.com>
2022-06-07 13:59:38 -0500

    cleaned up file structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>SVG Editing Tool</title>
+        <meta name="description" content="Tool for creating and editting svg files" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
