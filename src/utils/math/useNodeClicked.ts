fbf405b635fe41c1bf8b284c3073d07afe3b1f58
devalecf <afessler1998@gmail.com>
2022-10-26 18:05:28 -0500

    fixed bug from last commit

--- src/utils/math/useNodeClicked.ts
+++ src/utils/math/useNodeClicked.ts
@@ -3,9 +3,9 @@ export default function useNodeClicked(svgList: any[], x: number, y: number) {
     
     svgList.forEach((element) => {
         if (element.type === "selectedOutline") {
-           element.points.forEach((point: any) => {
-                if (point.x > x - 5 && point.x < x + 5 && point.y > y - 5 && point.y < y + 5) {
-                    nodeClicked = point;
+           element.points.forEach((node: any) => {
+                if (node.x > x - 5 && node.x < x + 5 && node.y > y - 5 && node.y < y + 5) {
+                    nodeClicked = node.point;
                 }
            });
         }
